e912e6695c4b10a8e2a2691cc784a7c91d068fde
Nathrixer <64125048@kmitl.ac.th>
2024-09-14 21:08:50 +0700

    Update todo.js

--- cell03/ex03bis/todo.js
+++ cell03/ex03bis/todo.js
@@ -1,1 +1,54 @@
+$(document).ready(function () {
+    function addTodoItem(todoText, appendToEnd = false) {
+        if (todoText.trim() === "") return;
 
+        const $todoItem = $('<div></div>').addClass('todo-item').text(todoText);
+
+        $todoItem.on('click', function() {
+            if (confirm("Do you want to remove this item?")) {
+                $todoItem.remove();
+                saveTodos();
+            }
+        });
+
+        if (appendToEnd) {
+            $('#ft_list').append($todoItem);
+        } else {
+            $('#ft_list').prepend($todoItem);
+        }
+
+        saveTodos();
+    }
+
+    $('#new-todo').on('click', function() {
+        const todoText = prompt("Enter your new TO-DO:");
+        if (todoText !== null && todoText.trim() !== "") {
+            addTodoItem(todoText);
+        }
+    });
+
+    function saveTodos() {
+        const todos = [];
+
+        $('#ft_list').children().each(function() {
+            todos.push($(this).text());
+        });
+
+        document.cookie = "todos=" + encodeURIComponent(JSON.stringify(todos)) + "; path=/";
+    }
+
+    function loadTodos() {
+        const cookies = document.cookie.split(';');
+        cookies.forEach(function(cookie) {
+            cookie = cookie.trim();
+            if (cookie.startsWith("todos=")) {
+                const todoList = JSON.parse(decodeURIComponent(cookie.substring("todos=".length)));
+                todoList.forEach(function(todoText) {
+                    addTodoItem(todoText, true);
+                });
+            }
+        });
+    }
+
+    loadTodos();
+});
